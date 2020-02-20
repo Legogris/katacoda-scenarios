c539132b3b309153c59fad8ff546dacdfb01de16
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2020-02-20 08:32:31 -0500

    trying assets

--- nomad-connect-acls/courseData.sh
+++ nomad-connect-acls/courseData.sh
@@ -1,3 +1,24 @@
+fix_journal() {
+  # This fixes an issue with the katacoda base machine that causes journald to
+  # fail on startup - H/T https://serverfault.com/a/800047
+  if [ ! -f "/etc/machine-id" ]
+  then
+    systemd-machine-id-setup
+    systemd-tmpfiles --create --prefix /var/log/journal
+    systemctl start systemd-journald.service
+  fi
+}
+
+install_cni_plugins() {
+
+  if [ ! -d "/opt/cni/bin" ]
+  then
+    curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.4/cni-plugins-linux-amd64-v0.8.4.tgz
+    sudo mkdir -p /opt/cni/bin
+    sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
+  fi
+}
+
 install_zip()
 {
   NAME="$1"
@@ -11,115 +32,18 @@ install_zip()
   fi
 }
 
+## main
+
+fix_journal
+
 install_zip "nomad" "https://releases.hashicorp.com/nomad/0.10.4-rc1/nomad_0.10.4-rc1_linux_amd64.zip"
 install_zip "consul" "https://releases.hashicorp.com/consul/1.7.0/consul_1.7.0_linux_amd64.zip"
 
-mkdir -p /etc/nomad.d /etc/consul.d
-mkdir -p /opt/nomad/data /opt/consul/data
-
-cat > /etc/nomad.d/config.hcl <<EOF
-data_dir  = "/opt/nomad/data"
-log_level = "DEBUG"
-
-client {
-  enabled = true
-}
-
-plugin "raw_exec" {
-  config {
-    enabled = true
-  }
-}
-
-server {
-  enabled          = true
-  bootstrap_expect = 1
-}
-EOF
-
-cat > /etc/consul.d/config.hcl <<EOF
-bind_addr        = "{{GetInterfaceIP \"ens3\"}}"
-bootstrap_expect = 1
-client_addr      = "0.0.0.0"
-data_dir         = "/opt/consul/data"
-datacenter       = "dc1"
-log_level        = "DEBUG"
-server           = true
-ui               = true
-
-connect {
-  enabled = true
-}
-
-ports {
-  grpc = 8502
-}
-EOF
-
-cat > /etc/systemd/system/nomad.service <<EOF
-[Unit]
-Description=Nomad
-Documentation=https://nomadproject.io/docs/
-Wants=network-online.target
-After=network-online.target
-StartLimitBurst=3
-StartLimitIntervalSec=10
-
-# When using Nomad with Consul it is not necessary to start Consul first. These
-# lines start Consul before Nomad as an optimization to avoid Nomad logging
-# that Consul is unavailable at startup.
-# Wants=consul.service
-# After=consul.service
-
-[Service]
-ExecReload=/bin/kill -HUP $MAINPID
-ExecStart=/usr/local/bin/nomad agent -config /etc/nomad.d
-KillMode=process
-KillSignal=SIGINT
-LimitNOFILE=65536
-LimitNPROC=infinity
-Restart=on-failure
-RestartSec=2
-TasksMax=infinity
-OOMScoreAdjust=-1000
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat > /etc/systemd/system/consul.service <<EOF
-[Unit]
-Description=Consul
-Documentation=https://consul.io/docs/
-Wants=network-online.target
-After=network-online.target
-StartLimitBurst=3
-StartLimitIntervalSec=10
-
-[Service]
-ExecReload=/bin/kill -HUP $MAINPID
-ExecStart=/usr/local/bin/consul agent -config-dir /etc/consul.d
-KillMode=process
-KillSignal=SIGINT
-LimitNOFILE=65536
-LimitNPROC=infinity
-Restart=on-failure
-RestartSec=2
-TasksMax=infinity
-
-[Install]
-WantedBy=multi-user.target
-EOF
+mkdir -p /etc/nomad.d /etc/consul.d /opt/nomad/data /opt/consul/data
 
 systemctl daemon-reload
-systemctl enable consul
-systemctl enable nomad
-systemctl start consul
-systemctl start nomad
-
-curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.4/cni-plugins-linux-amd64-v0.8.4.tgz
-sudo mkdir -p /opt/cni/bin
-sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
+systemctl enable consul nomad
+systemctl start consul nomad
 
 ln -s /etc/nomad.d
 ln -s /etc/consul.d
